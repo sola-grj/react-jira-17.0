0ad3308274e460adaca4f27cd5e5a20d698a1680
sola-grj <1131631886@qq.com>
2023-03-23 14:42:09 +0800

    添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { List } from "./list";
 import { useEffect, useState } from "react";
 import { cleanObject, useDebounce, useMount } from "utils";
 import * as qs from "qs";
+import { useHttp } from "utils/http";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -16,22 +17,27 @@ export const ProjectListScreen = () => {
   });
   const debounceParam = useDebounce(param, 200);
   const [list, setList] = useState([]);
+  const client = useHttp();
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
-      }
-    );
+    client("projects", {
+      data: cleanObject(debounceParam),
+    }).then(setList);
+    // fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(
+    //   async (response) => {
+    //     if (response.ok) {
+    //       setList(await response.json());
+    //     }
+    //   }
+    // );
   }, [debounceParam]);
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
+    // fetch(`${apiUrl}/users`).then(async (response) => {
+    //   if (response.ok) {
+    //     setUsers(await response.json());
+    //   }
+    // });
   });
   return (
     <div>
